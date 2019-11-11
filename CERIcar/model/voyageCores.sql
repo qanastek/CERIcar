3ef2deda29f9b3f3089dc308cee292dde200ca7a
Vougeot Valentin <uapv1804026@pedago01c.univ-avignon.fr>
2019-11-11 17:00:40 +0100

    Corespondace search is working

--- CERIcar/model/voyageCores.sql
+++ CERIcar/model/voyageCores.sql
@@ -1,23 +1,42 @@
-DROP FUNCTION IF EXISTS voyageCores(depart varchar, arrivee varchar);
+DROP FUNCTION IF EXISTS voyageCores(d_depart varchar, a_arrivee varchar);
 
-CREATE OR REPLACE FUNCTION voyageCores(depart varchar, arrivee varchar)
+CREATE OR REPLACE FUNCTION voyageCores(d_depart varchar, a_arrivee varchar)
 RETURNS VOID
 AS
 $$
 	DECLARE
 		my_record record;
         my_dep record;
+        heure float;
+
 	BEGIN
         FOR my_dep in select *
             FROM jabaianb.voyage join jabaianb.trajet on jabaianb.voyage.trajet = jabaianb.trajet.id
-            WHERE jabaianb.trajet.depart=depart
+            WHERE jabaianb.trajet.depart=d_depart
         LOOP
             raise notice 'test:%',my_dep;
 
+            FOR my_record in select *
+            FROM jabaianb.voyage join jabaianb.trajet on jabaianb.voyage.trajet = jabaianb.trajet.id
+            WHERE jabaianb.trajet.arrivee=a_arrivee
+
+            loop
+                IF my_dep.arrivee = my_record.depart Then
+
+                    
+                    heure = my_dep.heuredepart+(my_dep.distance/60);
+
+                    if heure <= my_record.heuredepart Then
+
+                    raise notice 'corespondance: % > %',my_dep,my_record;
+                    end if;
+                
+                END IF;
+            end loop;
         END LOOP;
 
 	END;
 $$
 LANGUAGE plpgsql;
 
-select voyageCOres('Paris','Lyon');
+select voyageCOres('Paris','Nice');

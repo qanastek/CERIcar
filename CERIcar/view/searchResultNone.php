9c2494c17f9e0c48fbaf53e47fc27a205b8e31d1
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-11-18 09:37:15 +0100

    Fix some bugs

--- CERIcar/view/searchResultNone.php
+++ CERIcar/view/searchResultNone.php
@@ -1,7 +1,5 @@
 <div class="centeredSearch">
 
-<?php include($nameApp . "/view/components/fromToBar.php"); ?>
-
 <div class="text-center">
     <img src="undraw_not_found_60pq.svg" style="width: 25em;">
     <h1 class="titleSearch">Pas de voyages trouver.</h1>

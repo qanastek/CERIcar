a5be548094b742ae1ee9c633ffb6cd2d50d21394
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-11-19 08:23:04 +0100

    update the login page

--- CERIcar/view/loginSuccess.php
+++ CERIcar/view/loginSuccess.php
@@ -1,16 +1,27 @@
-<div>
+<div class="row" style="height: 80vh; width: 100%;">
+        <div class="col-md-6">
 
-  <div class="form-group">
-    <label for="identifier">Identifiant</label>
-    <input type="text" class="form-control" id="identifier" placeholder="Entrer votre identifiant">
-  </div>
+        <div style="padding-left: 3%; padding-right: 3%;">
 
-  <div class="form-group">
-    <label for="password">Mot de passe</label>
-    <input type="password" class="form-control" id="password" placeholder="Mot de passe">
-  </div>
+            <div class="form-group">
+            <label for="identifier" class="title-login">Identifiant</label>
+            <input type="text" class="form-control" id="identifier" placeholder="Entrer votre identifiant">
+            </div>
 
-  <button class="btn btn-primary submit">Submit</button>
+            <div class="form-group">
+            <label for="password" class="title-login">Mot de passe</label>
+            <input type="password" class="form-control" id="password" placeholder="Mot de passe">
+            </div>
+
+            <button class="btn btn-primary submit" style="background-color: #29aff5; border-color: #29aff5;">Login</button>
+            <button class="btn btn-primary register" style="background-color: #29aff5; border-color: #29aff5;">Register</button>
+
+        </div>
+    </div>
+
+    <div class="col-md-6" style="padding: 0px;">
+        <img src="<?php echo $context->getImages($nameApp, 'login.jpg'); ?>" style="width: 100%; height: 100%; object-fit: cover; overflow: hidden;">
+    </div>
 
 </div>
 
@@ -37,6 +48,16 @@ $(".submit").click(function(){
                 $( "#statusBar" ).html(banner);
             });
         }
+    });
+});
+
+$(".register").click(function(){
+    $.get( "monApplicationAjax.php?action=register", function(data) {
+        $( "#mainContent" ).html( data );
+    });
+
+    $.get( "monApplicationAjax.php?action=banner", function(banner) {
+        $( "#statusBar" ).html( banner );
     });
 });
 </script>

8a868eeb74a8ae9ac083ea4194338994dfed8b6a
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-11-29 12:18:44 +0100

    - Ajout d'une image lorsque le client confirme sa réservation

--- CERIcar/view/bookSuccess.php
+++ CERIcar/view/bookSuccess.php
@@ -1,1 +1,8 @@
-Bravo vous venez de réserver votre voyage
+<div class="centeredSearch">
+
+<div class="text-center">
+    <img src="order_confirmed.svg" style="width: 25em;">
+    <h1 class="titleSearch">Réservation confirmer.</h1>
+</div>
+
+</div>

c5fd65e3d8f33b4435be1581fa58deb298660342
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-12-02 20:13:12 +0100

    - Fix la banner en deux deux

--- CERIcar/view/headerSuccess.php
+++ CERIcar/view/headerSuccess.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-white navbar-light bg-light">
 
-  <a class="navbar-brand">
+  <a class="navbar-brand home">
     <img src="<?php echo $context->getImages($nameApp, 'logo_cericar.PNG'); ?>" style="width: 75%;">
   </a>
 
@@ -9,88 +9,42 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
+    <ul class="navbar-nav ml-auto mr-0">
+        <li class="nav-item">
+            <a class="nav-link searchVoyage">
+                <i class="fas fa-search"></i>
+                Rechercher
+            </a>
+        </li>
+
+        <?php if(isset($_SESSION["user_id"])): ?>
+        <li class="nav-item">
+            <a class="nav-link offerSeats">
+                <i class="fas fa-plus-circle"></i>
+                Proposer un voyage
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link profile">
+                <i class="far fa-user-circle"></i>
+                <?php echo $context->getSessionAttribute('user_login'); ?>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link logout">Déconnexion</a>
+        </li>
+        <?php endif; ?>
+        
+        <?php if(!isset($_SESSION["user_id"])): ?>
+        <li class="nav-item">
+            <a class="nav-link register">Inscription</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link login">Connexion</a>
+        </li>
+        <?php endif; ?>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
-</nav>
-
-
-<nav class="navbar navbar-dark bg-white navbar-expand-lg">
-    
-    <a class="navbar-brand home">
-        <img src="<?php echo $context->getImages($nameApp, 'logo_cericar.PNG'); ?>" style="width: 75%;">
-    </a>
-
-    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#menuPrincipal">
-        <i class="fas fa-bars" style="color:  rgb(0, 175, 245);"></i>
-    </button>
-
-    <div class="collapse navbar-collapse" id="menuPrincipal">
-        <div class="nav navbar-nav navbar-collapse collapse w-100 order-2">
-            <ul class="navbar-nav ml-auto">
-
-                <li class="nav-item">
-                    <a class="nav-link searchVoyage">
-                        <i class="fas fa-search"></i>
-                        Rechercher
-                    </a>
-                </li>
-
-                <?php if(isset($_SESSION["user_id"])): ?>
-                <li class="nav-item">
-                    <a class="nav-link offerSeats">
-                        <i class="fas fa-plus-circle"></i>
-                        Proposer un voyage
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link profile">
-                        <i class="far fa-user-circle"></i>
-                        <?php echo $context->getSessionAttribute('user_login'); ?>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link logout">Déconnexion</a>
-                </li>
-                <?php endif; ?>
-                
-                <?php if(!isset($_SESSION["user_id"])): ?>
-                <li class="nav-item">
-                    <a class="nav-link register">Inscription</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link login">Connexion</a>
-                </li>
-                <?php endif; ?>
-
-            </ul>
-        </div>
-    </div>
-
 </nav>
 
 <script>
